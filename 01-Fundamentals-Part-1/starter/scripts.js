c5b5e42365a88312f761cc8f3c0485d31669ffe5
imramesh222 <imrameshrawat@gmail.com>
2024-05-31 23:12:22 +0545

    Changes to js

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -1,21 +1,29 @@
-let js='amazing';
-// if(js==='amazing') alert('javaScript is fun');
+// let js='amazing';
+// // if(js==='amazing') alert('javaScript is fun');
 
-x=12+3+90-20;
-console.log(x);
+// x=12+3+90-20;
+// console.log(x);
 
-console.log('Ramesh')
-let firstname="Ramesh"
+// console.log('Ramesh')
+// let firstname="Ramesh"
 
-console.log(firstname)
-let lastName='Rawat';
+// console.log(firstname)
+// let lastName='Rawat';
 
-let fullName = 'srish rawat'
-let PI=3.1415
-console.log(fullName)
+// let fullName = 'srish rawat'
+// let PI=3.1415
+// console.log(fullName)
 
-let myFirstJob='Programmer'
-let mySecondJob='Teacher'
+// let myFirstJob='Programmer'
+// let mySecondJob='Teacher'
 
-console.log(myFirstJob);
-console.log(mySecondJob);
+// console.log(myFirstJob);
+// console.log(mySecondJob);
+//2nd day of training
+// true;
+// console.log(true)
+let javaScriptIsFun=true;
+console.log(javaScriptIsFun);
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 3);
