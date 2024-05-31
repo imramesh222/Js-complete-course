b3da68a70abbf3d8a55eece253a548fc5674c2c9
imramesh222 <imrameshrawat@gmail.com>
2024-05-31 23:31:37 +0545

    changes to js

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -1,44 +1,54 @@
-// let js='amazing';
-// // if(js==='amazing') alert('javaScript is fun');
+// // let js='amazing';
+// // // if(js==='amazing') alert('javaScript is fun');
 
-// x=12+3+90-20;
-// console.log(x);
+// // x=12+3+90-20;
+// // console.log(x);
 
-// console.log('Ramesh')
-// let firstname="Ramesh"
+// // console.log('Ramesh')
+// // let firstname="Ramesh"
 
-// console.log(firstname)
-// let lastName='Rawat';
+// // console.log(firstname)
+// // let lastName='Rawat';
 
-// let fullName = 'srish rawat'
-// let PI=3.1415
-// console.log(fullName)
+// // let fullName = 'srish rawat'
+// // let PI=3.1415
+// // console.log(fullName)
 
-// let myFirstJob='Programmer'
-// let mySecondJob='Teacher'
+// // let myFirstJob='Programmer'
+// // let mySecondJob='Teacher'
 
-// console.log(myFirstJob);
-// console.log(mySecondJob);
-//2nd day of training
-// true;
-// console.log(true)
-let javaScriptIsFun=true;
+// // console.log(myFirstJob);
+// // console.log(mySecondJob);
+// //2nd day of training
+// // true;
+// // console.log(true)
+// let javaScriptIsFun=true;
+// // console.log(javaScriptIsFun);
+// // console.log(typeof true);
+// // console.log(typeof javaScriptIsFun);
+// // console.log(typeof 33);
+
+// javaScriptIsFun='yes';
 // console.log(javaScriptIsFun);
 // console.log(typeof true);
 // console.log(typeof javaScriptIsFun);
 // console.log(typeof 33);
 
-javaScriptIsFun='yes';
-console.log(javaScriptIsFun);
-console.log(typeof true);
-console.log(typeof javaScriptIsFun);
-console.log(typeof 33);
-
-let year;
-console.log(year);
-console.log(typeof year);
-year=1990;
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
+// year=1990;
+// console.log(typeof year);
 
 
-console.log(typeof null);
+// console.log(typeof null);
+
+let age=30;
+console.log(age);
+age = 32;
+console.log(age);
+console.log(typeof age);
+
+
+myName='Ramesh Rawat';
+console.log(myName);
