e1df55248af4653a3326f561485de3b2e599df41
imramesh222 <imrameshrawat@gmail.com>
2024-05-31 16:27:18 +0545

    Variables and reserved words learned

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -1,5 +1,20 @@
 let js='amazing';
-if(js==='amazing') alert('javaScript is fun');
+// if(js==='amazing') alert('javaScript is fun');
 
 x=12+3+90-20;
 console.log(x);
+
+console.log('Ramesh')
+let firstname="Ramesh"
+
+console.log(firstname)
+let lastName='Rawat';
+
+let name = 'srish rawat'
+let PI=3.1415
+
+let myFirstJob='Programmer'
+let mySecondJob='Teacher'
+
+console.log(myFirstJob);
+console.log(mySecondJob);
--- script1.js
+++
@@ -1,7 +0,0 @@
-      let js='amazing';
-      // if(js==='amazing') alert('javaScript is fun');
-
-      x=12+3+90-20;
-      console.log(x);
-
-      console.log('Jonas')
