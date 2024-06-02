e69b9c6de2d10a04f118652092d59d511edd4716
imramesh222 <imrameshrawat@gmail.com>
2024-06-02 11:26:48 +0545

    === and == operators

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -173,24 +173,39 @@
 
 //five falsy values: 0,'',undefined,null,NaN
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(''));
 // console.log(Boolean('Jonas'));
 // console.log(Boolean({}));
 
 
 
-const money=0;
-if (money){
-  console.log(`he doesnt have with him`);
-}else{
-  console.log(`he has  ${money} with him`);
-}
+// const money=0;
+// if (money){
+//   console.log(`he doesnt have with him`);
+// }else{
+//   console.log(`he has  ${money} with him`);
+// }
 
-let height;
-if(height){
-  console.log(`height is defined`);
-}else{
-  console.log(`height is not defined`);
-}
+// let height;
+// if(height){
+//   console.log(`height is defined`);
+// }else{
+//   console.log(`height is not defined`);
+// }
+
+//Equality operators : == vs ===
+
+// const age = 18;
+// if (age===18){
+//   console.log('He just been 18 years old');
+//   console.log(`he is ${age} years old`);
+// }else{
+//   console.log(`he is not 18 years old`);
+//   console.log(`he is ${age} years old`);
+// }
+
+const age = 18;
+if (age===18)
+  console.log('He just been 18 years old');
